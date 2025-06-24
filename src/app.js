efefe139604fa7ab26a5aeab1133b70c8aa28269
MdAthikHasan <mdathikhasan136@gmail.com>
2025-06-24 05:23:11 -0700

    cors origin updated

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", // not "*"
+    origin: "https://roadmapapp-front-end.vercel.app/",
     credentials: true,
   })
 );
